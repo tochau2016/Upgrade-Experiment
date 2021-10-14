28587aa2ae4698ae595ddad48f5e7556a9b49055
tochau2016 <tochau2016@gmail.com>
2021-10-15 04:45:45 +0700

    3d project

---
+++ halloweenpractice/main.js
@@ -0,0 +1,31 @@
+// ============ SHOW MENU ============
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+// ====== MENU SHOW ======
+// Kiểm tra biến có tồn tại không?
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+
+// ====== MENNU HIDDEN ======
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// ============ REMOVE MENU MOBILE ============
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+
+    // Khi click vào một nav - link.menu tự động ẩn đi
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
