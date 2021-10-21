2691c83855301c3fe1d43d302066fd8e7382ed07
tochau2016 <tochau2016@gmail.com>
2021-10-22 04:40:33 +0700

    home for halloween practice

--- halloweenpractice/js/main.js
+++ halloweenpractice/js/main.js
@@ -29,3 +29,14 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// ========= HOME SWIPER ===========
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
