1e117bf54c28e7c8d50a0aa723edad36c3f4329b
tochau2016 <tochau2016@gmail.com>
2021-10-23 05:53:29 +0700

    update halloween

--- halloweenpractice/js/main.js
+++ halloweenpractice/js/main.js
@@ -40,3 +40,15 @@ let homeSwiper = new Swiper(".home-swiper", {
         clickable: true
     }
 });
+
+// ============== CHANGE BACKGROUND HEADER =============
+function scrollHeader() {
+    const header = document.getElementById('header')
+        // Khi scroll hơn 50 đơn vị chiều cao thì thêm class  scroll-header vào header-tag
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
