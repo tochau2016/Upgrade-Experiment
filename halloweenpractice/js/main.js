de6b7bb3ea4a5a311ce4e0f47c5571254c764da3
tochau2016 <tochau2016@gmail.com>
2021-11-01 07:48:40 +0700

    done halloween

--- halloweenpractice/js/main.js
+++ halloweenpractice/js/main.js
@@ -52,3 +52,58 @@ function scrollHeader() {
     }
 }
 window.addEventListener('scroll', scrollHeader)
+
+// =============== NEW SWIPER =================
+let newSwiper = new Swiper(".new-swiper", {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    loop: 'true',
+});
+
+
+// ============= SCROLL SECTIONS ACTIVE LINK ===========
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// ================= SHOW SCROLL UP ===============
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // Khi scroll hơn 460 đơn vị chiều cao, thêm class show-scroll vào tag
+    if (this.scrollY >= 460) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollUp)
+
+// ================== SCROLL REVEAL ANIMATION ==============
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.home-swiper, .new-swiper, .newsletter__container')
+sr.reveal('.category__data, .trick__content, .footer__container', { interval: 100 })
+sr.reveal('.about__data, .discount__img', { origin: 'left' })
+sr.reveal('.about__img, .discount__data', { origin: 'right' })
