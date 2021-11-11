f38faf5e51e68524d9bf8a78da4d4136b61f22b1
tochau2016 <tochau2016@gmail.com>
2021-11-11 10:10:36 +0700

    question

--- plantpractice/js/main.js
+++ plantpractice/js/main.js
@@ -42,3 +42,31 @@ function scrollHeader() {
   }
 }
 window.addEventListener("scroll", scrollHeader);
+
+// ============ QUESTIONS ACCORDION ============
+const accordionItems = document.querySelectorAll(".question__item");
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".questions__header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+function toggleItem(item) {
+  const accordionContent = item.querySelector(".questions__content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+}
