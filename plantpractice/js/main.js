009565127c8b44fe01fa3efa419d1e5a37354fe6
tochau2016 <tochau2016@gmail.com>
2021-11-17 05:55:59 +0700

    done

--- plantpractice/js/main.js
+++ plantpractice/js/main.js
@@ -94,3 +94,69 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+// ============ SHOW SCROLL UP ==============
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // Khi scroll hon 200 don vi chieu cao, them class show-scroll
+  if (this.scrollY >= 200) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
+
+// ========== DARK LIGHT THEME ===========
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Trước khi chọn chủ đề (nếu người dùng đã chọn)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("select-icon");
+
+// Ta có được giao diện hiện tại do giao diện đang xác thực lớp dark-thêm
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// Ta kiểm tra nếu trước đó người dùng đã chọn chủ đề
+if (selectedTheme) {
+  // Nếu kiểm tra hoàn tất, xem xét vấn đề để biết rõ đã bật hay tắt darkmode
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Bật/Tắt theme với giao diện
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // Lưu theme và icon hiện tại mà người dùng chọn
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+// =========== SCROLL REVEAL ANIMATION ===========
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reaveal(`.home__data`);
+sr.reaveal(`.home__img`, { delay: 500 });
+sr.reaveal(`.home__social`, { delay: 600 });
+sr.reaveal(`.about__img, .contact__box`, { origin: "left" });
+sr.reaveal(`.about__data, .contact__form`, { origin: "right" });
+sr.reaveal(`.steps__card, .product__card, .questions__group, .footer`, {
+  interval: "100",
+});
