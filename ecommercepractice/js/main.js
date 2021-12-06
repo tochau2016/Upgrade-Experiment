5e48e5fd957d8d2a90981d1db4dab3bf782eb3cd
tochau2016 <tochau2016@gmail.com>
2021-12-06 17:52:31 +0700

    half ecommerce

--- ecommercepractice/js/main.js
+++ ecommercepractice/js/main.js
@@ -29,3 +29,32 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+// ============= TESTTIMONIAL SWIPER ==============
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// ========== NEW SWIPER ============
+let newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 24,
+  loop: "true",
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
