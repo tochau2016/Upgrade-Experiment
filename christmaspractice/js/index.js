cd8058b6e7f75b62ba34b223efc98950421bc1de
tochau2016 <tochau2016@gmail.com>
2021-12-19 23:29:24 +0700

    gift + celebrate + giving + home

---
+++ christmaspractice/js/index.js
@@ -0,0 +1,41 @@
+// ========== SHOW MENU ===========
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// =============== MENU SHOW ==============
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// ================ MENU CLOSE ================
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// ============ REMOVE MENU MOBILE ============
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((e) => e.addEventListener("click", linkAction));
+
+// ============== CHANGE BACKGROUND HEADER ============
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
