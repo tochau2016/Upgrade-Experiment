0491cd3685b1b87febef5f71900927673aa2deb8
tochau2016 <tochau2016@gmail.com>
2021-12-20 20:43:25 +0700

    footer

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-i = 0;
-function Test() {
-  if (i < 4) {
-    i++;
-  } else {
-    i = 0;
-  }
-  console.log(i);
-}
+// i = 0;
+// function Test() {
+//   if (i < 4) {
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+//   console.log(i);
+// }
 
-document.getElementById("next").addEventListener("click", Test);
+// document.getElementById("next").addEventListener("click", Test);
+
+let testBtn = document.querySelectorAll("thumbnail");
+
+// function testSth() {
+//   if (this === document.activeElement) {
+//     console.log("Button has been focus");
+//   } else {
+//     console.log("Nothing");
+//   }
+// }
+
+testBtn.forEach((item) => {});
+
+testBtn.forEach((e) => e.addEventListener("click", testSth));
