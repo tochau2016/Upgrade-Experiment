cb8d76b48019e4175314c7caa3cc267c21576efe
tochau2016 <tochau2016@gmail.com>
2021-12-20 20:45:16 +0700

    update footer

--- christmaspractice/js/index.js
+++ christmaspractice/js/index.js
@@ -39,3 +39,22 @@ function scrollHeader() {
   }
 }
 window.addEventListener("scroll", scrollHeader);
+
+// =============== NEW SWIPER ================
+var newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 24,
+  loop: "true",
+  slidesPerView: "auto",
+  centeredSlides: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    992: {
+      spaceBetween: 80,
+    },
+  },
+});
